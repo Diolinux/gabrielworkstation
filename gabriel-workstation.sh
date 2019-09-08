ff445f2f8f1533ca9895317a51c65c624da3d24f
Diolinux <Diolinux@users.noreply.github.com>
2019-09-08 16:07:34 -0300

    Add files via upload

---
+++ gabriel-workstation.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+## Removendo travas eventuais do apt ##
+
+sudo rm /var/lib/dpkg/lock-frontend && sudo rm /var/cache/apt/archives/lock
+
+## Atualizando o repositório ##
+
+&& sudo apt update 
+
+## Instalando pacotes e programas do repositório deb do Ubuntu ##
+
+&& sudo apt install python3 python-pip nautilus-dropbox docker docker-compose git build-essential libssl-dev flatpak gnome-software-plugin-flatpak 
+
+## Instalando pacotes Snap ##
+
+&& sudo snap install slack --classic  
+&& sudo snap install skype --classic  
+&& sudo snap install code --classic  
+&& sudo snap install --edge node --classic 
+&& sudo snap install insomnia  
+&& sudo snap install spotify
+&& sudo snap install wps-office-multilang 
+
+## Adicionando repositório Flathub ##
+
+&& flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
+
+## Instalando Apps do Flathub ##
+
+&& sudo flatpak install flathub com.obsproject.Studio -y 
+&& flatpak install flathub com.sublimetext.three -y
+&& flatpak install flathub io.dbeaver.DBeaverCommunity -y
+ 
+
+## Softwares que precisam de download externo ##
+
+cd /tmp && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb -y
+
+##Softwares alternativos Windows##
+
+##GIMP e PhotoGIMP
+&& flatpak install flathub org.gimp.GIMP && wget -c https://doc-0s-1g-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/0v83rmt4mij9897co9ufvor2r1jcj1am/1567965600000/07452089978596344616/*/12i-ihCDSZelx30-oNHJaKAzUei1etsbS?e=download && mv 12i-ihCDSZelx30-oNHJaKAzUei1etsbS?e=download phothogimp.zip && unzip photogimp.zip && cd "PHOTOGIMP V2018 - DIOLINUX" && cd "PATCH" && mkdir -p /home/$USER/.var/app/org.gimp.GIMP/config/GIMP/2.10/ && cp -R * /home/$USER/.var/app/org.gimp.GIMP/config/GIMP/2.10/
+
+## Wine softwares ###
+
+&& sudo apt install wine -y && wget -c https://uploads.treeunfe.me/downloads/instalar-freenfe.exe && wine instalar-freenfe.exe
+
+## Atualização do sistema ##
+
+&& sudo apt update && sudo apt dist-upgrade -y
+
+#Fim do Script ##
+
+&& echo "Finalizado"
